0a3192667d76fe2edc57d43a1fec0015a32b796d
Ioalevia <stihyary@gmail.com>
2023-01-16 20:40:34 +0300

    lesson_5

--- blog/config.py
+++ blog/config.py
@@ -7,7 +7,7 @@ class BaseConfig(object):
     SQLALCHEMY_DATABASE_URI = "sqlite:///blog.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "v0w_hu@=c65diol%(kp%h6ii&d4(3-vp38f8)2^6jd&qm&@%*+"
-
+    WTF_CSRF_ENABLED = True
 
 class DevConfig(BaseConfig):
     Debug = True
--- blog/views/auth.py
+++ blog/views/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, current_app
 from flask_login import (
     LoginManager,
     current_user,
@@ -7,7 +7,12 @@ from flask_login import (
     login_required,
 )
 
+from sqlalchemy.exc import IntegrityError
+from werkzeug.exceptions import NotFound
+
 from blog.models.user import UserModel
+from blog.models import db
+from blog.forms.user import RegistrationForm, LoginForm
 
 auth_app = Blueprint("auth_app", __name__)
 
@@ -27,16 +32,29 @@ def unauthorized():
 
 @auth_app.route("/login/", methods=["GET", "POST"], endpoint="login")
 def login():
-    if request.method == "GET":
-        return render_template("auth/login.html")
-    email = request.form.get("email")
-    if not email:
-        return render_template("auth/login.html", error="email not passed")
-    user = UserModel.query.filter_by(email=email).one_or_none()
-    if user is None:
-        return render_template("auth/login.html", error=f"login please")
-    login_user(user)
-    return redirect(url_for("index"))
+    if current_user.is_authenticated:
+        return redirect("index")
+
+    form = LoginForm(request.form)
+
+    if request.method == "POST" and form.validate_on_submit():
+        user = UserModel.query.filter_by(username=form.username.data).one_or_none()
+        if user is None:
+            return render_template("auth/login.html", form=form, error="username doesn't exist")
+        if not user.validate_password(form.password.data):
+            return render_template("auth/login.html", form=form, error="invalid username or password")
+
+        login_user(user)
+        return redirect(url_for("index"))
+
+    return render_template("auth/login.html", form=form)
+
+
+@auth_app.route("/login-as/", methods=["GET", "POST"], endpoint="login-as")
+def login_as():
+    if not (current_user.is_authenticated and current_user.is_staff):
+        # non-admin users should not know about this feature
+        raise NotFound
 
 
 @auth_app.route("/logout/", endpoint="logout")
@@ -45,6 +63,42 @@ def logout():
     logout_user()
     return redirect(url_for("index"))
 
+
+@auth_app.route("/register/", methods=["GET", "POST"], endpoint="register")
+def register():
+    if current_user.is_authenticated:
+        return redirect("index")
+
+    error = None
+    form = RegistrationForm(request.form)
+    if request.method == "POST" and form.validate_on_submit():
+        if UserModel.query.filter_by(username=form.username.data).count():
+            form.username.errors.append("username already exists!")
+            return render_template("auth/register.html", form=form)
+
+        if UserModel.query.filter_by(email=form.email.data).count():
+            form.email.errors.append("email already exists!")
+            return render_template("auth/register.html", form=form)
+
+        user = UserModel(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            username=form.username.data,
+            email=form.email.data,
+            is_staff=False,
+        )
+        user.password = form.password.data
+        db.session.add(user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            current_app.logger.exception("Could not create user!")
+            error = "Could not create user!"
+        else:
+            current_app.logger.info("Created user %s", user)
+            login_user(user)
+            return redirect(url_for("index"))
+    return render_template("auth/register.html", form=form, error=error)
 
 
 __all__ = [
